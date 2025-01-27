821a4b3818b03ad8c87ce79f5bc3e5b92ec9e95d
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2025-01-26 23:18:55 -0300

    Update: headerCadastroAluno.js
    Refatoração: enviarAlteracao precisa chamar aluno e ID.

--- src/components/personal/headerCadastroAluno/headerCadastroAluno.js
+++ src/components/personal/headerCadastroAluno/headerCadastroAluno.js
@@ -1,26 +1,24 @@
 import { useState } from "react";
 import Popup from "../popupConectaAluno/popupConectaAluno";
-import { Personalapi } from '../../../api/personalApi';
-import './headerCadastroAluno.css'; 
+import { Personalapi } from "../../../api/personalApi";
+import "./headerCadastroAluno.css";
 
 const HeaderCadastroAluno = ({ onAlunoCadastrado }) => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const [aluno, setAluno] = useState('');
+  const [aluno, setAluno] = useState("");
 
   const togglePopup = () => setIsPopupOpen(!isPopupOpen);
 
-  const Iid = localStorage.getItem('@id');
+  const Iid = localStorage.getItem("@id");
   const ID = JSON.parse(Iid);
 
   const enviarAlteracao = async (e) => {
     e.preventDefault();
     try {
-      await Personalapi.alteracao(ID);
-      setAluno('');
+      await Personalapi.alteracao(aluno, ID);
+      setAluno("");
       onAlunoCadastrado();
-    } catch (err) {
-      console.error(err);
-    }
+    } catch (err) {}
   };
 
   return (
