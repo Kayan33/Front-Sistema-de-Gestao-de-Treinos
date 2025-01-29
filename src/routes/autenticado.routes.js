225285c3a99a21458e8bfbb08a3bbb8073bd4a66
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2025-01-29 17:09:18 -0300

    Update autenticado.routes.js
    Routes AlunoUnico

--- src/routes/autenticado.routes.js
+++ src/routes/autenticado.routes.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DashBoardPersonal from "../pages/personal/DashBoardPersonal/DashBoardPersonal";
-import ListaAlunos from "../pages/ListaAlunos/ListaAlunos";
+import ListaAlunos from "../pages/personal/ListaAlunos/ListaAlunos";
+import AlunoUnico from "../pages/personal/AlunoUnico/AlunoUnico";
+
 
 export default function Autentificado() {
   return (
@@ -9,6 +11,7 @@ export default function Autentificado() {
         <Route path="/DashBoardPersonal" element={<DashBoardPersonal/>}/>
         <Route path="/" element={<DashBoardPersonal/>}/>
         <Route path="/cliente/lista" element={<ListaAlunos/>}/>
+        <Route path="/cliente/:aluno" element={<AlunoUnico/>}/>
       </Routes>
     </BrowserRouter>
   )
