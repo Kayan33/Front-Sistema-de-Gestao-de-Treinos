24bc325a75034f37226be41e55256c2698ad99fe
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2025-01-31 17:20:03 -0300

    Create: TrocaSenha.js

---
+++ src/pages/personal/TrocaSenha/TrocaSenha.js
@@ -0,0 +1,60 @@
+import React, {  useState } from 'react';
+import { Link, useNavigate, useParams, } from 'react-router-dom';
+import { EmailAPI } from '../../../api/EmailApi';
+
+
+
+export default function TrocaSenha() {
+  
+  const [senha, setSenha] = useState('');
+  const [Confirmarsenha, setConfirmarsenha] = useState('');
+
+  const { token } = useParams();
+const navigate = useNavigate()
+
+  async function dadosLogin(e) {
+    e.preventDefault();
+    await EmailAPI.TrocaSenha(senha,token)
+  
+    navigate("/")
+  }
+
+  return (
+    <div className="container-pai-cadastro-login-personal">
+      <div className="login-cadastro-personal-form-container">
+        <h2 >Troca senha</h2>
+        <form className="login-personal-form"  onSubmit={dadosLogin}>
+          
+          <div className="login-personal-input-group">
+            <label htmlFor="senha" className="login-cadastro-personal-label">Nova senha</label>
+            <input
+              type="password"
+              id="senha"
+              className="login-cadastro-personal-input-select"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="login-personal-input-group">
+            <label htmlFor="senha" className="login-cadastro-personal-label">Confirmar senha</label>
+            <input
+              type="password"
+              id="senha"
+              className="login-cadastro-personal-input-select"
+              value={Confirmarsenha}
+              onChange={(e) => setConfirmarsenha(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className="login-personal-btn">Entrar</button>
+        </form>
+
+        <p className="login-personal-register-link">
+           <Link to="/" className="login-personal-link">Login</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
