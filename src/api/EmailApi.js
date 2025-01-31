9e904607d3f7f289b81e3d2032a0bc9234032f27
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2025-01-31 17:18:45 -0300

    Create: EmailApi.js

---
+++ src/api/EmailApi.js
@@ -0,0 +1,39 @@
+import { toast } from "react-toastify";
+import apiLocal from "./apilocal/apiLocal";
+
+export const EmailAPI = {
+  EnvioEmail: async (email) => {
+    try {
+      const resposta = await apiLocal.post(`/esqueci-senha`, { email });
+
+      toast.success(
+        "E-mail enviado! Entre no seu e-mail para redefinir a senha."
+      );
+      return resposta;
+    } catch (err) {
+      console.error("Erro ao consultar treino:", err);
+
+      toast.error(
+        err.response?.data?.error || "Erro desconhecido. Tente novamente."
+      );
+      throw err;
+    }
+  },
+
+  TrocaSenha: async (senha, token) => {
+    try {
+      const resposta = await apiLocal.put(`/resetar-senha/${token}`, {
+        senha,
+      });
+      toast.success("Troca de senha concluida");
+      return resposta;
+    } catch (err) {
+      console.error("Erro ao consultar treino:", err);
+
+      toast.error(
+        err.response?.data?.error || "Erro desconhecido. Tente novamente."
+      );
+      throw err;
+    }
+  },
+};
