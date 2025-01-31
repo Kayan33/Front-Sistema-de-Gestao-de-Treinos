8d2a6c0420d0a1af5ea2ec4c5401833482244f76
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2025-01-31 17:21:01 -0300

    Create : EsqueciSenha.css
    Implementa página de "Esqueci Senha" com envio de e-mail para troca de senha

---
+++ src/pages/personal/EsqueciSenha/EsqueciSenha.js
@@ -0,0 +1,45 @@
+import React, { useContext, useState } from 'react';
+import { Link, useNavigate, } from 'react-router-dom';
+import { AutenticadoContexto } from '../../../context/authContexts';
+import { EmailAPI } from '../../../api/EmailApi';
+
+export default function EsqueciSenha() {
+  const [email, setEmail] = useState('');
+
+  const navigate = useNavigate()
+
+  async function TrocaSenha(e) {
+    e.preventDefault();
+    
+  await EmailAPI.EnvioEmail(email)
+  navigate("/")
+
+  }
+
+  return (
+    <div className="container-pai-cadastro-login-personal">
+      <div className="login-cadastro-personal-form-container">
+        <h2 >Esqueci senha</h2>
+        <p>Será enviado um e-mail para troca de senha</p>
+        <form className="login-personal-form" onSubmit={TrocaSenha} >
+          <div className="login-personal-input-group">
+            <label htmlFor="email" className="login-cadastro-personal-label">E-mail</label>
+            <input
+              type="email"
+              id="email"
+              className="login-cadastro-personal-input-select"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          
+          <button type="submit" className="login-personal-btn">Entrar</button>
+        </form>
+        <p className="login-personal-register-link">
+           <Link to="/" className="login-personal-link">Login</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
