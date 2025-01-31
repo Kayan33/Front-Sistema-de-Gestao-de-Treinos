218d2c8340c594d482dd760b42183c36c8ac72f3
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2025-01-31 17:21:44 -0300

    Update :nAutenticado.routes.js
    feito rotas EsqueciSenha e TrocaSenha.

--- src/routes/nAutenticado.routes.js
+++ src/routes/nAutenticado.routes.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPersonal from "../pages/personal/LoginPersonal/LoginPersonal";
 import CadastroPersonal from "../pages/personal/CadastroPersonal/CadastroPersonal";
+import EsqueciSenha from "../pages/personal/EsqueciSenha/EsqueciSenha";
+import TrocaSenha from "../pages/personal/TrocaSenha/TrocaSenha";
 
 export default function Nautentificado() {
 
@@ -9,6 +11,8 @@ export default function Nautentificado() {
       <Routes>
         <Route path="/"  element={<LoginPersonal/>}/>
         <Route path="/cadastro/personal"  element={<CadastroPersonal/>}/>
+        <Route path="/resetar-senha"  element={<EsqueciSenha/>}/>
+        <Route path="/Troca-senha/:token"  element={<TrocaSenha/>}/>
       </Routes>
     </BrowserRouter>
   )
