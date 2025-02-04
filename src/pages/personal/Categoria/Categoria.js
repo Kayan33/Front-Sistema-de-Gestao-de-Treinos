95c94fa7178e0626a65ea475eec2628373eac2f3
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2025-02-04 17:17:15 -0300

    Create: Adicionar funcionalidade de busca e exibição de categorias
    - Implementa a busca filtrada de categorias através do hook `useBusca`.
    - Conecta o componente à API de categorias (`CategoriaApi`) para obter as categorias cadastradas.
    - Exibe uma lista de categorias filtradas, com links para a página de detalhes de cada categoria.
    - Adiciona input de busca para facilitar a localização de categorias.
    - Verificação de token de autenticação antes de carregar os dados.

---
+++ src/pages/personal/Categoria/Categoria.js
@@ -0,0 +1,59 @@
+import { useContext, useEffect, useState } from "react";
+import Header from "../../../components/personal/header/header";
+import { AutenticadoContexto } from "../../../context/authContexts";
+import { CategoriaApi } from "../../../api/CategoriaApi";
+import { Link } from "react-router-dom";
+import "./Categoria.css";
+import { useBusca } from "../../../hook/useBusca/useBusca";
+
+export default function Categoria() {
+  const [categoria, setCategoria] = useState([]);
+  const { listaFiltrada, termoBusca, setTermoBusca } = useBusca(categoria, ["categoria",]);
+
+  const { verificarToken, token } = useContext(AutenticadoContexto);
+  verificarToken();
+
+  useEffect(() => {
+    consultaCategoria();
+  }, []);
+
+  async function consultaCategoria() {
+    try {
+      const resposta = await CategoriaApi.consulta();
+      setCategoria(resposta.data);
+    } catch (error) {}
+  }
+
+  return (
+    <div className="dashboard-personal-container">
+      <Header />
+      <div className="container-lista-aluno">
+          <form className="form-busca-aluno">
+            <input
+              type="text"
+              className="input-busca-aluno"
+              placeholder="Nome, E-mail, Telefone"
+              required
+              value={termoBusca}
+              onChange={(e) => setTermoBusca(e.target.value)}
+            />
+          </form>
+        <div className="constainer-Link-categoria">
+          {listaFiltrada.length > 0 ? (
+            listaFiltrada.map((cat) => (
+              <Link
+                key={cat.id}
+                className="Link-categoria"
+                to={`/exercicio/${cat.id}`}
+              >
+                <h3 className="Link-categoria-h3">{cat.categoria}</h3>
+              </Link>
+            ))
+          ) : (
+            <p>Sem categoria cadastrada.</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
