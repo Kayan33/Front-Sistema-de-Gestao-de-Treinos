702b7c8d6a663d30c5f6055c322143559d1f8deb
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2025-02-07 11:58:16 -0300

    feat: adicionar funcionalidade de cadastro, exclusão e visualização de vídeo para exercícios

--- src/pages/personal/Exercicio/Exercicio.js
+++ src/pages/personal/Exercicio/Exercicio.js
@@ -1,45 +1,136 @@
-import { Await, data, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Header from "../../../components/personal/header/header";
+import { useEffect, useState, useContext } from "react";
+import { AutenticadoContexto } from "../../../context/authContexts";
+import { useBusca } from "../../../hook/useBusca/useBusca";
+import "./Exercicio.css";
+import PopupCadastrarExercicio from "../../../components/personal/PopupCadastrarExercicio/PopupCadastrarExercicio";
+import PopupVideio from "../../../components/personal/popupVideio/popupVideio";
+import { ExercicioApi } from "../../../api/ExercicioApi";
 import { CategoriaApi } from "../../../api/CategoriaApi";
-import { useEffect, useState } from "react";
 
 export default function Exercicio() {
+  const [exercicio, setExercicio] = useState(null);
+  const [isPopupCadastroOpen, setIsPopupCadastroOpen] = useState(false);
+  const [isPopupVideoOpen, setIsPopupVideoOpen] = useState(false);
+  const [selectedExercicio, setSelectedExercicio] = useState(null);
+  const [nome_exercicio, setNome_exercicio] = useState("");
+  const [URL_video, setURL_video] = useState("");
 
-    const[exercicio, setExercicio]= useState(null)
+  const { categoriaID } = useParams();
+  const { verificarToken, token } = useContext(AutenticadoContexto);
 
-    const{categoria}=useParams()
+  verificarToken();
 
-   async function ConsultaUnica() {
-      const resposta =  await CategoriaApi.consultaUnica(categoria)
-          setExercicio(resposta.data)    
+  async function ConsultaUnica() {
+    try {
+      const resposta = await CategoriaApi.consultaUnica(categoriaID);
+      setExercicio(resposta.data);
+    } catch (error) {
+      console.error("Erro ao buscar exercícios:", error);
     }
+  }
 
-    useEffect(()=>{
-        ConsultaUnica()
-    },[categoria])
+  useEffect(() => {
+    ConsultaUnica();
+  }, [categoriaID]);
 
-    return(
-        <div className="dashboard-personal-container">
-            <Header/>
-            <div className="container-aluno-unico">
+  async function CadastrarExercicio() {
+    await ExercicioApi.cadastro(nome_exercicio, URL_video, categoriaID);
+    setNome_exercicio("");
+    setURL_video("");
+    await ConsultaUnica();
+  }
 
-           {exercicio ?
-           (<div>
-            <h1>{exercicio.categoria}</h1>
-                <ul>
-                    {exercicio.exercicios.map((item)=>(
-                        <li key={item.id}>
-                            {item.nome_exercicio}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            ):(
-                <p>ddd</p>
-            )
-        }
+  async function DeleteExercicio(ID) {
+    await ExercicioApi.delete(ID);
+    await ConsultaUnica();
+  }
 
-            </div>
+  const { listaFiltrada, termoBusca, setTermoBusca } = useBusca(
+    exercicio ? exercicio.exercicios : [],
+    ["nome_exercicio"]
+  );
+
+  return (
+    <div className="dashboard-personal-container">
+      <Header />
+      <div className="container-aluno-unico">
+        <div className="container-aluno-unico-links">
+          <form className="form-busca-aluno">
+            <input
+              type="text"
+              className="input-busca-aluno"
+              placeholder="Nome do exercício"
+              required
+              value={termoBusca}
+              onChange={(e) => setTermoBusca(e.target.value)}
+            />
+          </form>
+
+          <PopupCadastrarExercicio
+            isOpen={isPopupCadastroOpen}
+            togglePopup={() => setIsPopupCadastroOpen(!isPopupCadastroOpen)}
+            onSubmit={CadastrarExercicio}
+            nome_exercicio={nome_exercicio}
+            setNome_exercicio={setNome_exercicio}
+            URL_video={URL_video}
+            setURL_video={setURL_video}
+          />
+
+          <button
+            className="BTN-adiciona"
+            onClick={() => setIsPopupCadastroOpen(true)}
+          >
+            Cadastrar Exercício
+          </button>
         </div>
-    )
+
+        {exercicio ? (
+          <div className="exercicios-wrapper">
+            <h1 className="titulo-exercicios">{exercicio.categoria}</h1>
+            {listaFiltrada.length > 0 ? (
+              <ul className="lista-exercicios">
+                {listaFiltrada.map((item) => (
+                  <li key={item.id} className="item-exercicio">
+                    <p>{item.nome_exercicio}</p>
+
+                    <a
+                      className="link-exercicio"
+                      onClick={() => {
+                        setSelectedExercicio(item);
+                        setIsPopupVideoOpen(true);
+                      }}
+                    >
+                      Ver vídeo
+                    </a>
+                    <button
+                      className="BTN-remove"
+                      onClick={() => DeleteExercicio(item.id)}
+                    >
+                      -
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p className="msg-nenhum-exercicio">
+                Nenhum exercício encontrado.
+              </p>
+            )}
+          </div>
+        ) : (
+          <p>Carregando...</p>
+        )}
+      </div>
+
+      {isPopupVideoOpen && selectedExercicio && (
+        <PopupVideio
+          isOpen={isPopupVideoOpen}
+          togglePopup={() => setIsPopupVideoOpen(false)}
+          exercicio={[selectedExercicio]}
+        />
+      )}
+    </div>
+  );
 }
