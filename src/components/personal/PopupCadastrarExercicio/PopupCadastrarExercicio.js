aed08c2dbfbea8da6392b1cd355ca27fd8e89495
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2025-02-08 15:48:55 -0300

    feat: adicionar popup para cadastro de exercícios

---
+++ src/components/personal/PopupCadastrarExercicio/PopupCadastrarExercicio.js
@@ -0,0 +1,64 @@
+import "../../../style/classes.css";
+import "./PopupCadastrarExercicio.css";
+import { toast } from "react-toastify";
+
+const PopupCadastrarExercicio = ({
+  isOpen,
+  togglePopup,
+  onSubmit,
+  nome_exercicio,
+  setNome_exercicio,
+  URL_video,
+  setURL_video,
+}) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!nome_exercicio || !URL_video) {
+      toast.error("Preencha todos os campos!");
+      return;
+    }
+    onSubmit({ nome_exercicio, URL_video });
+  };
+
+  return (
+    isOpen && (
+      <div className="popup-overlay" onClick={togglePopup}>
+        <div className="popup-content" onClick={(e) => e.stopPropagation()}>
+          <button className="close-button" onClick={togglePopup}>
+            &times;
+          </button>
+
+          <div className="cadastrar-aluno-container">
+            <form className="cadastrar-aluno-form" onSubmit={handleSubmit}>
+              <h2>Cadastrar Exercício</h2>
+
+              <input
+                type="text"
+                className="form-input-number"
+                placeholder="Nome Exercicio"
+                value={nome_exercicio}
+                onChange={(e) => setNome_exercicio(e.target.value)}
+                required
+              />
+
+              <input
+                type="text"
+                className="form-input-number"
+                placeholder="URL do vídeo"
+                value={URL_video}
+                onChange={(e) => setURL_video(e.target.value)}
+                required
+              />
+
+              <button type="submit" className="form-button">
+                Cadastrar
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default PopupCadastrarExercicio;
