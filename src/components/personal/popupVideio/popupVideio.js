7cc5bc121f62d18e6f912f3d9345e6df957480f5
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2025-02-13 16:29:04 -0300

    feat: css de videio e link de videio

--- src/components/personal/popupVideio/popupVideio.js
+++ src/components/personal/popupVideio/popupVideio.js
@@ -1,3 +1,5 @@
+import'./popupVideio.css';
+
 const PopupVideio = ({
   isOpen,
   togglePopup,
@@ -6,17 +8,17 @@ const PopupVideio = ({
     return(
         isOpen && (
             <div className="popup-overlay" onClick={togglePopup}>
-              <div className="popup-content" onClick={(e) => e.stopPropagation()}>
+              <div className="popup-content-video" onClick={(e) => e.stopPropagation()}>
                 <button className="close-button" onClick={togglePopup}>
                   &times;
                 </button>
       
-                <div className="cadastrar-aluno-container">
+                <div className="cadastrar-aluno-container ">
 
                   {exercicio.map((item)=>(
                    
 
-                  <iframe src={item.URL_video}
+                  <iframe src={`https://www.youtube.com/embed/${item.URL_video}`}
                   className="exercicio-video"
                   title={item.nome_exercicio}></iframe>
                   ))}
