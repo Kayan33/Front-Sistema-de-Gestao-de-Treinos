9a3dbb5597ec701a23d474efc88b5f0ddce9202c
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2025-02-16 11:30:59 -0300

    Feat: nome do personal

--- src/components/personal/header/header.js
+++ src/components/personal/header/header.js
@@ -1,24 +1,34 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import './header.css'; 
-import { AutenticadoContexto } from '../../../context/authContexts';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import "./header.css";
+import { AutenticadoContexto } from "../../../context/authContexts";
 
 export default function Header() {
-const{logout} = useContext(AutenticadoContexto)
+  const { logout } = useContext(AutenticadoContexto);
+
+  const Inome = localStorage.getItem("@nome");
+  const nome = Inome ? JSON.parse(Inome) : null;
+
+  const primeiroNome = nome ? nome.split(" ")[0] : "";
 
   return (
-    
-      <header className="header-personal">
-        <h1>Bem-vindo </h1>
-        <div className="navigation-personal">
-          <Link to="/assinatura" className='navigation-personal-link'>Assinatura</Link>
-          <Link to="/ajuda" className='navigation-personal-link'>Ajuda</Link>
-          <Link to="/perfil" className='navigation-personal-link'>Perfil</Link>
+    <header className="header-personal">
+      <h1>Bem-vindo {primeiroNome}</h1>
+      <div className="navigation-personal">
+        <Link to="/assinatura" className="navigation-personal-link">
+          Assinatura
+        </Link>
+        <Link to="/ajuda" className="navigation-personal-link">
+          Ajuda
+        </Link>
+        <Link to="/perfil" className="navigation-personal-link">
+          Perfil
+        </Link>
 
-        <Link className='logout' onClick={logout} >logout</Link>
-        </div>
-      </header>
-
-    
+        <Link className="logout" onClick={logout}>
+          logout
+        </Link>
+      </div>
+    </header>
   );
 }
